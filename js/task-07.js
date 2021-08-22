b3c591191870db253145a17f1b5542c56854c14b
ITgrand <vitaliidovhopolyk@gmail.com>
2021-08-23 02:04:47 +0300

    task-07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+
+
+const inputEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+inputEl.addEventListener('input', event => {
+    const size = event.target.value;
+    textEl.style.fontSize = `${size}px`;
+});
